2eb8608ad967299cd3b473c148a3e0d690212688
Vivian Chen <vivian.chen1193@gmail.com>
2022-09-21 15:35:57 -0600

    required files that will be used for the applicaton, began working on user inputs for the readme

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-// TODO: Include packages needed for this application
+// Included packages needed for this application by requiring it
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'Please input your Github username'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please input your email address'
+    },
+    {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'Please input the title of your project'
+    },
+    {
+        type: 'input',
+        name: 'descr',
+        message: 'Please write your project a thorough description of your project'
+    },
+    {
+        type: 'input',
+        name: 'GitHub',
+        message: 'Please input your Github username.'
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'Please pick a license you would like to use for your project',
+        choices: ['MIT', 'APACHE', 'GPL','NONE']
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
